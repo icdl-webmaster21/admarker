9436e49c8ade237348de7f3e05f56a6c0343ccf8
NawMarMar <68273064+NawMarMar@users.noreply.github.com>
2021-07-20 14:09:37 +0630

    update service tab jquery

--- js/src/app.js
+++ js/src/app.js
@@ -12,19 +12,69 @@ $('.sub-menu-02 li a').hover(
 );
 
 //servie details tab jquery
+$(document).ready(function() {
+    $('.nav-tabs a').click(function() {
+
+        $('.tabs-inn').hide();
+        $('.nav-tabs a.active').removeClass('active');
+        $(this).addClass('active');
+
+        var panel = $(this).attr('href');
+        $(panel).fadeIn(1000);
+
+        return false; // prevents link action
+
+    }); // end click 
+    $('.nav-tabs li:first a').click();
+
+});
+
+// $(function() {
+//     var activeTab = document.querySelector(location.hash);
+//     if (activeTab) {
+//       activeTab.click();
+//     }
+//   });
+
+
 // $(document).ready(function() {
-//     $('.tabs a').click(function() {
 
-//         $('.service-tab').hide();
-//         $('.tabs a.active').removeClass('active');
-//         $(this).addClass('active');
+//     //When page loads...
+//     $(".tabs-content .tabs-inn").hide(); //Hide all content
+//     $(".tabs li:first a").addClass("active").show(); //Activate first tab
+//     $(".tabs-content .tabs-inn:first").show(); //Show first tab content
 
-//         var panel = $(this).attr('href');
-//         $(panel).fadeIn(1000);
+//     //On Click Event
+//     $(".tabs li a").click(function() {
 
-//         return false; // prevents link action
+//         $(".tabs li a").removeClass("selected"); //Remove any "active" class
+//         $(this).addClass("selected"); //Add "active" class to selected tab
+//         $(".tabs-content .tabs-inn").hide(); //Hide all tab content
 
-//     }); // end click 
-//     $('.tabs li:first a').click();
+//         var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+//         $(activeTab).fadeIn(); //Fade in the active ID content
+//         $(".tabs-content .tabs-inn").show();
+//         return true;
+//     });
+
 
 // });
+
+var showTab = function(selector) {
+    $('.tabs-inn').hide()
+    $(selector).show();
+}
+
+$('.tabs a').click(function(event) {
+    var $el = $(event.target).closest('a')
+    var id = $el.attr('id')
+    showTab('#' + id)
+});
+
+$(function() {
+    var selector = window.location.hash
+    if (selector) {
+        showTab(selector)
+    }
+    // $('.nav-tabs a').addClass('active');
+})
